47151074d2d5a27c2bf1de8644243c637df8fad7
Lorena <lorena.pantano@gmail.com>
2015-08-23 10:08:21 -0400

    add ref and hint of commands for data generation

--- R/isomiRexp-data.R
+++ R/isomiRexp-data.R
@@ -9,9 +9,13 @@
 #' @usage 
 #' data("isomiRexp")
 #' @source
-#' bla bla
+#' Data was download from GEO dataset and analyzed with seqbuster tool.
+#' adrec file adapter 8 1 34
+#' miraligner file databse hsa 1 3 3 prefix
 #' @references 
-#' bla bla
+#' Mehmet Somel et al. MicroRNA, mRNA, and protein expression link 
+#' development and aging in human and macaque brain. 
+#' Genome Research, 20(9):1207–1218, 2010.doi:10.1101/gr.106849.110.
 #' @format a \code{IsomirDataSeq} class.
 #' @author Lorena Pantano, 2015-05-19
 NULL
