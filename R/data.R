931e1c2179906ecf07bb6036ad47b684364f4645
Lorena Pantano <lorena.pantano@gmail.com>
2017-05-14 21:33:07 -0500

    add source docs for network data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Data frame containing gene-mirna relationship
+#'
+#' @format A data frame with rows sames as gene_ex_rse and
+#' columns same as mirna_ex_rse.
+"ma_ex"
+
+#' Data frame containing gene expression data
+#'
+#' @format gene expression data with 18 samples: 
+#' example of a time series data
+"gene_ex_rse"
+
+#' Data frame containing mirna expression data
+#'
+#' @format mirna expression data with 18 samples: 
+#' example of a time series data
+"mirna_ex_rse"
