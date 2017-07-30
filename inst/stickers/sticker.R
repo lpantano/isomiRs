eae0987742b2daac647124c016192f6c1458a532
Lorena Pantano <lorena.pantano@gmail.com>
2017-07-30 19:42:35 -0400

    better sticker colors [ci]

--- inst/stickers/sticker.R
+++ inst/stickers/sticker.R
@@ -15,7 +15,7 @@ metadata$condition <- metadata[,column]
         mutate(t5tag=ifelse(t5 != "0", "Yes", "No")) %>%
         mutate(t3tag=ifelse(t3 != "0", "Yes", "No"))
     .s <- data.frame(sample=sample,
-                     type=c("mism","add","t5","t3","ref"),
+                     type=c("mm","add","t5","t3","ref"),
                      method="freq",
                      freq=c(sum(.d$freq[.d$mismtag=="Yes"], na.rm=TRUE),
                             sum(.d$freq[.d$addtag=="Yes"], na.rm=TRUE),
@@ -26,7 +26,7 @@ metadata$condition <- metadata[,column]
     )
     .s$freq <- .s$freq/sum(.s$freq) * 100
     .u <- data.frame(sample=sample,
-                     type=c("mism","add","t5","t3","ref"),
+                     type=c("mm","add","t5","t3","ref"),
                      method="unique",
                      freq=c(sum(.d$mismtag=="Yes"),
                             sum(.d$addtag=="Yes"),
@@ -43,28 +43,28 @@ p<-do.call(rbind, .l) %>% arrange(type) %>%
     ggplot(aes_string(x="type", y="freq", group="sample", color="condition")) +
     geom_polygon(fill=NA, size=1) +
     coord_polar(start=-pi) +
-    #scale_color_brewer(palette = "Set1") +
+    scale_color_manual(values = c("purple4", "purple4")) +
     theme_bw() + ylab("") + xlab("") + guides(color=FALSE) +
     theme(panel.grid.major = element_line(size = 1)) +
     theme(axis.title.x=element_blank(),
           axis.ticks=element_blank(),
-          axis.text.x = element_text(size=15, face = "bold", color="orange"),
           panel.border = element_blank(),
           axis.text.y = element_blank(),
           plot.background = element_rect(fill = "transparent",colour = NA),
-          panel.background = element_rect(fill = "transparent",colour = NA)) +
+          panel.background = element_rect(fill = "transparent",colour = NA),
+          axis.text.x = element_text(size=25, face = "bold", color="orange")) +
     scale_y_continuous(expand = c(0,0))
 
 hexSticker::sticker(p,
         package="isomiRs", 
-        s_x=0.95,
+        s_x=0.93,
         s_y=0.8, 
-        s_width=1.2, 
-        s_height=1.5,
+        s_width=1.5, 
+        s_height=1.8,
         p_x = 1,
-        p_y = 1.5,
+        p_y = 1.6,
         h_color = "orange",
-        h_fill = "steelblue4",
+        h_fill = "steelblue",
         p_color = "orange",
         p_size = 16,
         filename="inst/stickers/isomirs.png")
