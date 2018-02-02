4aa47aa0815fb7a87fb71d66956757fee5607382
Lorena Pantano <lorena.pantano@gmail.com>
2018-02-02 04:15:04 -0500

    Move corrections functions to scounts pkg

--- R/isomiRs-package.R
+++ R/isomiRs-package.R
@@ -12,12 +12,7 @@
 #' @import GGally
 #' @import gtools
 #' @import grDevices
-#' @import gamlss
 #' @import Biobase
-#' @import TMB
-#' @import RcppEigen
-#' @importFrom gamlss.dist checklink
-#' @importFrom lme4 glmer fixef ranef
 #' @importFrom reshape melt melt.array melt.data.frame melt.list
 #' @importFrom tidyr spread
 #' @importFrom readr read_tsv
@@ -27,11 +22,13 @@
 #' @importFrom grDevices colorRampPalette
 #' @importFrom RColorBrewer brewer.pal
 #' @importFrom gplots heatmap.2
-#' @importFrom ggplot2 aes aes_string element_rect geom_jitter ggplot element_text
-#'           labs ggtitle xlab ylab scale_size facet_wrap
-#'           scale_color_brewer scale_colour_brewer theme theme_bw
-#'           stat_smooth coord_polar element_blank ggplot_gtable ggplot_build
-#'           geom_text geom_line geom_point ggplotGrob geom_polygon
+#' @importFrom ggplot2 aes aes_string element_rect geom_jitter 
+#'             ggplot element_text
+#'             labs ggtitle xlab ylab scale_size facet_wrap
+#'             scale_color_brewer scale_colour_brewer theme theme_bw
+#'             stat_smooth coord_polar element_blank
+#'             ggplot_gtable ggplot_build
+#'             geom_text geom_line geom_point ggplotGrob geom_polygon
 #' @importFrom gridExtra grid.arrange arrangeGrob
 #' @importFrom grid textGrob unit unit.pmax
 #' @importFrom graphics pairs
